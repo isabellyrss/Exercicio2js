37abec286ff92dc4deada2ca795eb63dd7925204
Isabelly Ramos dos Santos <isabelly.ramos.santos@escola.pr.gov.br>
2026-02-24 09:47:44 -0300

    Add files via upload

---
+++ Média_Se.js
@@ -0,0 +1,14 @@
+let Aluno="Robson";
+let N1=99;
+let N2=100;
+let N3=99;
+let N4=100;
+let media=0;
+let result="";
+media=(N1 + N2 + N3 + N4)/4;
+if (media >=70){
+    result="Aprovado"
+} else {
+    result="Reprovado"
+}
+console.log("O aluno" + Aluno + "tem a média" + media + "e está:" + result);
